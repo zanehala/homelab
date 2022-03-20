e424c7a96d21353bf96c661387504ec1df42a780
zanehala <zmhala@gmail.com>
2022-03-20 12:37:07 -0500

    Use zanelab org

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "homelab"
+    organization = "zanelab"
 
     workspaces {
       name = "homelab-external"
