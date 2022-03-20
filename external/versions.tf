181ee8329f6a191e917b5bd8de278d6332fa12d8
zanehala <zmhala@gmail.com>
2022-03-20 11:32:54 -0500

    Update with my domain/values

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "homelab"
 
     workspaces {
       name = "homelab-external"
--- platform/vault/files/generate-secrets/go.mod
+++ platform/vault/files/generate-secrets/go.mod
@@ -1,4 +1,4 @@
-module git.khuedoan.com/khuedoan/homelab/vault/init
+module git.zanelab.com/khuedoan/homelab/vault/init
 
 go 1.17
 
