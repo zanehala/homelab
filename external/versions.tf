e8a83bf44493cffc9c2dddb4724818f1a141a705
zanehala <zmhala@gmail.com>
2022-03-20 12:47:30 -0500

    Use kube config path

--- external/versions.tf
+++ external/versions.tf
@@ -44,6 +44,7 @@ provider "cloudflare" {
 # }
 
 provider "kubernetes" {
+  config_path = "../metal/kubeconfig.yaml"
   # Use KUBE_CONFIG_PATH environment variables
   # Or in cluster service account
 }
